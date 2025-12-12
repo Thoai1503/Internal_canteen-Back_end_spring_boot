3f7b4f6aa89ae5ec00a2f79c7bd05fcc1aed11e2
Thoai1503 <vothoai1503@gmail.com>
2025-12-12 17:33:48 +0700

    update db

--- src/main/java/tinhvomon/com/db/ConnectDataSource.java
+++ src/main/java/tinhvomon/com/db/ConnectDataSource.java
@@ -12,12 +12,19 @@ public class ConnectDataSource {
 	private static SQLServerDataSource ds;
 
     static {
-        ds = new SQLServerDataSource();
-        ds.setUser("sa");
-        ds.setPassword("123");
-        ds.setServerName("localhost");
+        // ds = new SQLServerDataSource();
+        // ds.setUser("sa");
+        // ds.setPassword("123");
+        // ds.setServerName("localhost");
+        // ds.setPortNumber(1433);
+        // ds.setDatabaseName("ProductDB");       
+        // ds.setTrustServerCertificate(true); // 
+         ds = new SQLServerDataSource();
+        ds.setUser("giangthoai2000_SQLLogin_1");
+        ds.setPassword("qsbnp3w54h");
+        ds.setServerName("ShoeShop.mssql.somee.com");
         ds.setPortNumber(1433);
-        ds.setDatabaseName("ProductDB");       
+        ds.setDatabaseName("ShoeShop");       
         ds.setTrustServerCertificate(true); // 
     }
 
